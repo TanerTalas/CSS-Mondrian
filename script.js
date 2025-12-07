0e94b9a1ccfde05981c4134deec8caa374ab3fdc
TanerTalas <r7byx0@gmail.com>
2025-12-08 01:43:39 +0300

    Homepage created. Hero, about and footer sections created with css html and js.

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".art-showcase-item");
+    let index = 0;
+
+    function cycle() {
+        items.forEach(el => el.classList.remove("active"));
+        items[index].classList.add("active");
+
+        index = (index + 1) % items.length;
+    }
+
+    cycle();
+    setInterval(cycle, 4500);
+});
